64a1dc6e6d15d68b089ef002cf491046f991ffaa
Thomas Couchoud <zerderr@gmail.com>
2015-12-22 01:21:55 +0100

    Handle switching video in playlist

--- src/js/hookerUtils.js
+++ src/js/hookerUtils.js
@@ -9,16 +9,24 @@ function changeDOMInfos(event){
     document.getElementById(YTT_DOM_PLAYER_INFOS).innerHTML = event[YTT_DURATION_EVENT_ID_KEY] + YTT_DOM_SPLITTER + event[YTT_DURATION_EVENT_DURATION_KEY];
 }
 
+function changeVideo() {
+    if(YTTGetPlayer().getVideoData && YTTGetPlayer().getVideoData()['video_id'] != (document.getElementById(YTT_DOM_PLAYER_INFOS).innerHTML.split(YTT_DOM_SPLITTER)[0] || '') && YTTGetPlayer().getCurrentTime && YTTGetPlayer().getDuration) {
+        changeDOMTime(2);
+        var event = {};
+        event[YTT_DURATION_EVENT_ID_KEY] = YTTPlayer.getVideoData()['video_id'];
+        event[YTT_DURATION_EVENT_DURATION_KEY] = YTTPlayer.getDuration();
+        changeDOMInfos(event);
+        YTTLog('Player rehooked');
+    }
+}
+
 function YTTGetPlayer(){
     return YTTPlayer;
 }
 
 function hookYTTPlayer(player)
 {
-    console.log(player);
-    if(YTTHooked || typeof player !== 'object')
-        return false;
-    if(!(player.getCurrentTime || player.getData || player.getDuration))
+    if(YTTHooked || typeof player !== 'object' || !(player.getCurrentTime || player.getVideoData || player.getDuration))
         return false;
     YTTLog("Player hooked");
     YTTHooked = true;
@@ -31,5 +39,6 @@ function hookYTTPlayer(player)
     changeDOMInfos(baseInfos);
     changeDOMTime(YTTPlayer.getPlayerState());
     YTTPlayer.addEventListener('onStateChange', changeDOMTime);
+    YTTPlayer.addEventListener('onApiChange', changeVideo);
     return true;
 }
