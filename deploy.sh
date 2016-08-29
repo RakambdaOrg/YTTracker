ff6e4a1389c49a320b61444213e4072a41f542e2
Thomas Couchoud <zerderr@gmail.com>
2016-08-29 14:44:37 +0200

    Should fix regex on deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-echo "$TRAVIS_BRANCH";
-if echo "$TRAVIS_BRANCH" | grep -q "^[0-9]\{1,\}[0-9]\{1,\}[0-9]$"; then
+if echo "$TRAVIS_BRANCH" | grep -q "^[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}$"; then
     npm run release;
 else
     npm run releaseTest;
